20805dc311853657081881418f870bdb73d3ca16
Donald Sonn <dsonn@Donalds-MBP.fios-router.home>
2019-03-02 12:40:16 -0500

    implemented search function

--- index.js
+++ index.js
@@ -16,23 +16,38 @@ function formatQueryParams(params) {
 
 function displayResults(responseJson) {
   // if there are previous results, remove them
-  console.log(responseJson);
-  $('#results-list').empty();
   // iterate through the items array
-  for (let i = 0; i < responseJson.items.length; i++){
+
+
+  for (let i = 0; i < responseJson.data.length; i++){
     // for each video object in the items 
     //array, add a list item to the results 
     //list with the video title, description,
     //and thumbnail
     $('#results-list').append(
-      `<li><h3>${responseJson.items[i].snippet.title}</h3>
-      <p>${responseJson.items[i].snippet.description}</p>
-      <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
-      </li>`
+      `<li><h3>${responseJson.data[i].name}</h3>
+      <p>${responseJson.data[i].description}</p>
+      <a href='${responseJson.data[i].url}'>${responseJson.data[i].name}</a>
+      `
     );}
   //display the results section  
   $('#results').removeClass('hidden');
+
+
 }
+
+// description: "Rainbow Bridge is one of the world's largest known natural bridges. The span has undoubtedly inspired people throughout time--from the neighboring American Indian tribes who consider Rainbow Bridge sacred, to the 85,000 people from around the world who visit it each year.↵↵Please visit Rainbow Bridge in a spirit that honors and respects the cultures to whom it is sacred."
+// designation: "National Monument"
+// directionsInfo: "Rainbow Bridge National Monument is located between Glen Canyon National Recreation Area and the Navajo Nation. There are no roads in the vicinity of the monument. Rainbow Bridge can be reached by boat on Lake Powell or by hiking one of two trails around Navajo Mountain on the Navajo Nation, by permit only. Boat tours are available.↵↵The entrance to Forbidding Canyon is located at buoy 42 on Lake Powell. Boaters should be familiar with the Aids to Navigation (buoy) system and use a navigational map."
+// directionsUrl: "http://www.nps.gov/rabr/planyourvisit/directions.htm"
+// fullName: "Rainbow Bridge National Monument"
+// id: "CF02A7E7-147F-4F1C-966B-FE3F2AE28DDC"
+// latLong: "lat:37.07788771, long:-110.9655099"
+// name: "Rainbow Bridge"
+// parkCode: "rabr"
+// states: "UT"
+// url: "https://www.nps.gov/rabr/index.htm"
+// weatherInfo: "Summers are extremely hot with little, if any, shade. Winters are moderately cold with night time lows often bel
 
 function getParksList(query, maxResults=10) {
 
@@ -43,21 +58,21 @@ function getParksList(query, maxResults=10) {
   for(let x in STORE.statesSelected) {
     statesURL.push(`stateCode=${STORE.statesSelected[x]}`);
   }
-  statesURL.join('&');
+  statesURL = statesURL.join('&');
 
-  console.log(npsURL+statesURL);
+  let url = npsURL+statesURL;
 
-  // fetch(url)
-  //   .then(response => {
-  //     if (response.ok) {
-  //       return response.json();
-  //     }
-  //     throw new Error(response.statusText);
-  //   })
-  //   .then(responseJson => displayResults(responseJson))
-  //   .catch(err => {
-  //     $('#js-error-message').text(`Something went wrong: ${err.message}`);
-  //   });
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
 }
 
 function watchForm() {
@@ -184,6 +199,10 @@ function watchForm() {
       if (this.checked)
         STORE.statesSelected.push($(this).val());
     });
+    if(STORE.statesSelected.length === 0) {
+      alert('Please select at least one state');
+      return;
+    }
     const searchTerm = $('#js-search-term').val();
     const maxResults = $('#js-max-results').val();
     getParksList(searchTerm, maxResults);
