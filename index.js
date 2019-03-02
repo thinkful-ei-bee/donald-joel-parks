f4ac98d91c460ca9a0b58fdc9f872bbaa2b0f8d1
Donald Sonn <dsonn@Donalds-MBP.fios-router.home>
2019-03-02 12:24:04 -0500

    adding state buttons using js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const STORE = {
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
   return queryItems.join('&');
 }
 
@@ -29,37 +29,35 @@ function displayResults(responseJson) {
       <p>${responseJson.items[i].snippet.description}</p>
       <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
       </li>`
-    )};
+    );}
   //display the results section  
   $('#results').removeClass('hidden');
-};
+}
 
-function getYouTubeVideos(query, maxResults=10) {
-  const params = {
-    key: apiKey,
-    q: query,
-    part: 'snippet',
-    maxResults,
-    type: 'video'
-  };
-  const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString;
+function getParksList(query, maxResults=10) {
 
-  console.log(url);
+  let npsURL = 'https://api.nps.gov/api/v1/parks?';
 
+  let statesURL = [];
 
-  
-  fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => displayResults(responseJson))
-    .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    });
+  for(let x in STORE.statesSelected) {
+    statesURL.push(`stateCode=${STORE.statesSelected[x]}`);
+  }
+  statesURL.join('&');
+
+  console.log(npsURL+statesURL);
+
+  // fetch(url)
+  //   .then(response => {
+  //     if (response.ok) {
+  //       return response.json();
+  //     }
+  //     throw new Error(response.statusText);
+  //   })
+  //   .then(responseJson => displayResults(responseJson))
+  //   .catch(err => {
+  //     $('#js-error-message').text(`Something went wrong: ${err.message}`);
+  //   });
 }
 
 function watchForm() {
@@ -70,6 +68,116 @@ function watchForm() {
   //   .then(myJson => console.log(myJson));
   // console.log(url);
 
+  let stateNames = ['Alabama',
+  'Alaska',
+  'Arizona',
+  'Arkansas',
+  'California',
+  'Colorado',
+  'Connecticut',
+  'Delaware',
+  'Florida',
+  'Georgia',
+  'Hawaii',
+  'Idaho',
+  'Illinois',
+  'Indiana',
+  'Iowa',
+  'Kansas',
+  'Kentucky',
+  'Louisiana',
+  'Maine',
+  'Maryland',
+  'Massachusetts',
+  'Michigan',
+  'Minnesota',
+  'Mississippi',
+  'Missouri',
+  'Montana',
+  'Nebraska',
+  'Nevada',
+  'New Hampshire',
+  'New Jersey',
+  'New Mexico',
+  'New York',
+  'North Carolina',
+  'North Dakota',
+  'Ohio',
+  'Oklahoma',
+  'Oregon',
+  'Pennsylvania',
+  'Rhode Island',
+  'South Carolina',
+  'South Dakota',
+  'Tennessee',
+  'Texas',
+  'Utah',
+  'Vermont',
+  'Virginia',
+  'Washington',
+  'West Virginia',
+  'Wisconsin',
+  'Wyoming'];
+  let stateAbbr = ['AL',
+  'AK',
+  'AZ',
+  'AR',
+  'CA',
+  'CO',
+  'CT',
+  'DE',
+  'FL',
+  'GA',
+  'HI',
+  'ID',
+  'IL',
+  'IN',
+  'IA',
+  'KS',
+  'KY',
+  'LA',
+  'ME',
+  'MD',
+  'MA',
+  'MI',
+  'MN',
+  'MS',
+  'MO',
+  'MT',
+  'NE',
+  'NV',
+  'NH',
+  'NJ',
+  'NM',
+  'NY',
+  'NC',
+  'ND',
+  'OH',
+  'OK',
+  'OR',
+  'PA',
+  'RI',
+  'SC',
+  'SD',
+  'TN',
+  'TX',
+  'UT',
+  'VT',
+  'VA',
+  'WA',
+  'WV',
+  'WI',
+  'WY']
+
+  let stateButtonList = [];
+  
+  for(let i = 0; i < stateAbbr.length; i++){
+    stateButtonList.push(`<input type="checkbox" name='state' value="${stateAbbr[i]}">${stateNames[i]}<br>`);
+  }
+  stateButtonList.join('');
+
+  $('.js-state-button-list').html(stateButtonList);
+
   $('form').submit(event => {
     event.preventDefault();
     $('input[type=checkbox]').each(function(){
@@ -78,7 +186,7 @@ function watchForm() {
     });
     const searchTerm = $('#js-search-term').val();
     const maxResults = $('#js-max-results').val();
-    getYouTubeVideos(searchTerm, maxResults);
+    getParksList(searchTerm, maxResults);
   });
 }
 
